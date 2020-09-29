13161d80ea785a43e1c8a6426341da3cc80c6d8b
jacob williams <jacdavwill@gmail.com>
2020-09-29 08:41:30 -0600

    added basic vehicle type

---
+++ types/vehicle.ts
@@ -0,0 +1,13 @@
+export interface Vehicle {
+  year: number
+  nickname: string
+  model: string
+  mileage: number
+  make: string
+  color: string
+  transmission: string
+  image: string
+  lastOilChangeDate: string
+  lastTireRotationDate: string
+  lastRegistrationDate: string
+}
